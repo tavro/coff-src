b899d900ffe7a4d1e80217a4c9a7b46d5b68a49f
Isak Horvath <isakhorvath@gmail.com>
2024-08-08 17:05:44 +0200

    ✨ COFF-01: 'Finish' lexer, Add entry point for user

--- src/coff/main.go
+++ src/coff/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"coff-src/src/coff/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! I'm the CoffLang interpreter. :-)\n", user.Username)
+	fmt.Printf("I'm ready to take your commands!\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+
