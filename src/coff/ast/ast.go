317459bfb5b8bd534a0ff042ec03228c42b1eb9b
Isak Horvath <isakhorvath@gmail.com>
2024-08-09 16:12:57 +0200

    ✨ COFF-02: Add ast test

--- src/coff/ast/ast.go
+++ src/coff/ast/ast.go
@@ -2,10 +2,12 @@ package ast
 
 import (
 	"coff-src/src/coff/token"
+	"bytes"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -21,6 +23,54 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (ds DefStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ds.TokenLiteral() + " ")
+	out.WriteString(ds.Name.String())
+	out.WriteString(" = ")
+
+	if ds.Value != nil {
+		out.WriteString(ds.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *RetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	
+	if rs.RetValue != nil {
+		out.WriteString(rs.RetValue.String())
+	}
+
+	out.WriteString(";")
+	
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	
+	return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
@@ -54,3 +104,12 @@ type RetStatement struct {
 
 func (rs *RetStatement) statementNode() {}
 func (rs *RetStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
---
+++ src/coff/ast/ast_test.go
@@ -0,0 +1,26 @@
+import (
+	"coff-src/src/coff/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&DefStatement{
+				Token: token.Token{Type: token.DEF, Literal: "def"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.ID, Literal: "thisVar"}
+					Value: "thisVar"
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.ID, Literal: "thatVar"}
+					Value: "thatVar"
+				},
+			},
+		},
+	}
+
+	if program.String() != "def thisVar = thatVar;" {
+		t.Errorf("program.String() is incorrect. got=%q", program.String())
+	}
+}
